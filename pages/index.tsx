854101c01b36e8929e909d8b8320009e822dc37f
alix <garrisonnichols7@gmail.com>
2023-02-21 10:03:52 -0500

    add animated gradient thingy + email

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import Link from "next/link"
 
-import { Github, Twitter } from "lucide-react"
+import { Github, Twitter, Mail, Clipboard } from "lucide-react"
+import { AnimatePresence, motion } from "framer-motion"
 
 function Header({ title } : { title: any}) {
   return (
@@ -22,20 +23,58 @@ const socials = [
   }
 ]
 
+const variants = {
+  inital: { y: 60 },
+  visible: { y: 0 },
+  exit: { y: 60 }
+}
+
 const Home = () => {
+  const [showCopied, setShowCopied] = useState(false)
+
   return (
     <>
+    <div className="flex justify-center absolute bottom-0 w-screen my-5">
+        <AnimatePresence
+          initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <motion.div 
+            initial="initial" 
+            exit="exit" 
+            animate={showCopied ? "visible" : "exit"} 
+            transition={{ type: "spring", duration: 0.5 }} 
+            variants={variants}
+            className="text-white flex items-center gap-1 py-2 px-4 font-medium bg-gray-900 bg-opacity-40 rounded-full backdrop-blur-md">
+            <Clipboard size={18} strokeWidth={2.2} />
+            <h1>Copied to clipboard!</h1>
+          </motion.div>
+        </AnimatePresence>
+      </div>
     <div className="bg-black flex flex-col items-center sm:flex-row justify-center gap-20 w-screen h-screen p-2">
     <div>
+      <div className="bg-gradient-to-r from-pink-600 to-purple-500 via-rose-500 animate-gradient-x justify-center items-center flex flex-col content-center 
+      auto rounded-md shadow-xl box-border h-20 -32 p-4 m-9 border-2 border-black">
+        <p className='text-white text-2xl text-center font-larsseitbold'>(｡•̀ᴗ-)✧</p>
+      </div>
       <h1 className="text-4xl text-center text-white font-larsseitbold sm:text-7xl tracking-tighter">hi! i'm alix</h1>
       <h2 className="text-2xl text-center text-white font-larsseit">tired artist and web developer <br />
       gamer on the side :3</h2>
-      <div className="flex items-center justify-center gap-2 mt-1">
+      <div className="flex text-white items-center justify-center gap-2 mt-1">
         {socials.map(x=>
           <a key={x.name} href={x.link}>
             <x.icon size={28}/>
           </a>
           )}
+          <Mail onClick={() => {
+              navigator.clipboard.writeText("alix@kaedehara.xyz")
+              setShowCopied(true)
+
+              setInterval(() => {
+                setShowCopied(false)
+              }, 5000)
+
+            }} size={28} className="cursor-pointer" />
       </div>
     </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,60 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-},
     extend: {
+
       fontFamily: {
         larsseit: ["larsseit"],
         larsseitbold: ["larsseitbold"],
         larsseitbolditalic: ["larsseitbolditalic"],
         larsseititalic: ["larsseititalic"]
-      }
-    },
+      },
+
+      'animation': {
+        'gradient-x': 'gradient-x 10s ease infinite',
+        'gradient-y':'gradient-y 10s ease infinite',
+        'gradient-xy':'gradient-xy 10s ease infinite',
+      },
+      
+      'keyframes': {
+        'gradient-y': {
+            '0%, 100%': {
+                'background-size':'400% 400%',
+                'background-position': 'center top'
+            },
+
+            '50%': {
+                'background-size':'200% 200%',
+                'background-position': 'center center'
+            }
+        },
+
+        'gradient-x': {
+            '0%, 100%': {
+                'background-size':'200% 200%',
+                'background-position': 'left center'
+            },
+
+            '50%': {
+                'background-size':'200% 200%',
+                'background-position': 'right center'
+            }
+        },
+
+        'gradient-xy': {
+            '0%, 100%': {
+                'background-size':'400% 400%',
+                'background-position': 'left center'
+            },
+
+            '50%': {
+                'background-size':'200% 200%',
+                'background-position': 'right center'
+            }
+        }
+      },
+    }
+  },
   plugins: [
     require('tailwindcss-bg-patterns')
   ],
